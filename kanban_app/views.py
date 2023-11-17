847489f8515e41adeb335bd65017a028276d733b
Evans Maina <82137970+Fabiandebug@users.noreply.github.com>
2023-11-17 16:53:50 +0300

    Final Commit

---
+++ kanban_app/views.py
@@ -0,0 +1,40 @@
+# kanban_app/views.py
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.renderers import JSONRenderer 
+from .models import Column, Card
+from .serializers import ColumnSerializer, CardSerializer
+from graphene_django.views import GraphQLView
+
+class ColumnListView(APIView):
+    renderer_classes = [JSONRenderer]  
+
+    def get(self, request, format=None):
+        columns = Column.objects.all()
+        serializer = ColumnSerializer(columns, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = ColumnSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class CardListView(APIView):
+    renderer_classes = [JSONRenderer] 
+
+    def get(self, request, format=None):
+        cards = Card.objects.all()
+        serializer = CardSerializer(cards, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = CardSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+graphql_view = GraphQLView.as_view(graphiql=True)
